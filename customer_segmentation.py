b54aab98e4123832f38b931b0829d70bbd7b1124
Ihsan Satriawan <ihsan.satriawan.20@gmail.com>
2017-04-14 09:56:25 +0700

    Read data from cdn

--- customer_segmentation.py
+++ customer_segmentation.py
@@ -1,4 +1,7 @@
 import sys
+import csv
+import urllib
+import pandas as pd
 
 def pct_rank_qcut(series, n):
   edges = pd.Series([float(i) / n for i in range(n + 1)])
@@ -6,7 +9,9 @@ def pct_rank_qcut(series, n):
   return series.rank(pct=1).apply(f)
 
 def normalize_data():
-  # Normalize data with pct_rank_qcut method
+  link = "https://gist.githubusercontent.com/ihsansatriawan/70fa30c1eb366223fd54a1a2462d4fd3/raw/ec0b9d5ffe415e3d7fd2b7ffc9c7c7919b77418c/sample_data.csv"
+  f = urllib.urlopen(link)
+  datas = f.read()
 
 def main(argv):
   normalize_data()
